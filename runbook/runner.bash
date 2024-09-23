fe9afa8915f9e9ad38744d2fd4e0c97a19d5929e
jimurrito <jimurrito@gmail.com>
2024-09-22 18:18:48 -0700

    updated test

--- runbook/runner.bash
+++ runbook/runner.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --name minceraft -p 25565:25565 -v ./.minceraft-data:/server jimurrito/minceraft:$(date "+%Y%m%d")
+docker run -it --rm -p 25565:25565 -v ./.minceraft-data:/server jimurrito/minceraft:$(date "+%Y%m%d")
