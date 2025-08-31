43b9a11f32bdefe62db16cddc872da722bc171da
jimurrito <jimurrito@gmail.com>
2025-08-31 13:33:17 -0500

    Temporarily removed arm support until openJDK21 update is made to support ARM as well

--- runbook/pusher.bash
+++ runbook/pusher.bash
@@ -4,13 +4,17 @@ if [[ -z "$(docker buildx ls | grep mince-builder)" ]]; then
     docker buildx create --name mince-builder --driver docker-container --use --bootstrap
 fi
 
+#
+# TEMP REMOVED
+#  #--platform linux/amd64,linux/arm64 \
+#
+
 # Build one instance per version in ./versions
 VERSIONS=$(cat "./versions")
 
 for v in $VERSIONS; do
     echo "Building image for Minecraft [$v]"
     docker buildx build --pull --push \
-        --platform linux/amd64,linux/arm64 \
         --build-arg "MC_VERSION=$v" \
         -t "jimurrito/minceraft:$v$(date "+%Y%m%d")" \
         -t "jimurrito/minceraft:$v" .
@@ -20,6 +24,5 @@ LATEST=${VERSIONS[0]}
 echo "Building image for Minecraft [$v] (Latest)"
 # latest label is the first version on the list
 docker buildx build --pull --push \
-    --platform linux/amd64,linux/arm64 \
     --build-arg "MC_VERSION=$LATEST" \
     -t jimurrito/minceraft:latest .
